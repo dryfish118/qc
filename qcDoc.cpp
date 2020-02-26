db5fd8b3a9d2af809e8e5612633d48ad8a28a2b7
dryfish118 <dryfish118@163.com>
2020-02-26 20:16:24 +0800

    init

---
+++ qcDoc.cpp
@@ -0,0 +1,199 @@
+﻿
+// qcDoc.cpp: CqcDoc 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "qc.h"
+#endif
+
+#include "qcDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CqcDoc
+
+IMPLEMENT_DYNCREATE(CqcDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CqcDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CqcDoc 构造/析构
+
+CqcDoc::CqcDoc() noexcept
+{
+	// TODO: 在此添加一次性构造代码
+}
+
+CqcDoc::~CqcDoc()
+{
+}
+
+static bool getSinglePlusSingle(CStringArray& formulas)
+{
+	const CString formula_single_plus_single[] =
+	{
+		L"1 x 1 =       ",	L"1 x 2 =       ",	L"2 x 1 =       ",	L"1 x 3 =       ",
+		L"3 x 1 =       ",	L"1 x 4 =       ",	L"4 x 1 =       ",	L"1 x 5 =       ",
+		L"5 x 1 =       ",	L"1 x 6 =       ",	L"6 x 1 =       ",	L"1 x 7 =       ",
+		L"7 x 1 =       ",	L"1 x 8 =       ",	L"8 x 1 =       ",	L"1 x 9 =       ",
+		L"9 x 1 =       ",	L"2 x 2 =       ",	L"2 x 3 =       ",	L"3 x 2 =       ",
+		L"2 x 4 =       ",	L"4 x 2 =       ",	L"2 x 5 =       ",	L"5 x 2 =       ",
+		L"2 x 6 =       ",	L"6 x 2 =       ",	L"2 x 7 =       ",	L"7 x 2 =       ",
+		L"2 x 8 =       ",	L"8 x 2 =       ",	L"2 x 9 =       ",	L"9 x 2 =       ",
+		L"3 x 3 =       ",	L"3 x 4 =       ",	L"4 x 3 =       ",	L"3 x 5 =       ",
+		L"5 x 3 =       ",	L"3 x 6 =       ",	L"6 x 3 =       ",	L"3 x 7 =       ",
+		L"7 x 3 =       ",	L"3 x 8 =       ",	L"8 x 3 =       ",	L"3 x 9 =       ",
+		L"9 x 3 =       ",	L"4 x 4 =       ",	L"4 x 5 =       ",	L"5 x 4 =       ",
+		L"4 x 6 =       ",	L"6 x 4 =       ",	L"4 x 7 =       ",	L"7 x 4 =       ",
+		L"4 x 8 =       ",	L"8 x 4 =       ",	L"4 x 9 =       ",	L"9 x 4 =       ",
+		L"5 x 5 =       ",	L"5 x 6 =       ",	L"6 x 5 =       ",	L"5 x 7 =       ",
+		L"7 x 5 =       ",	L"5 x 8 =       ",	L"8 x 5 =       ",	L"5 x 9 =       ",
+		L"9 x 5 =       ",	L"6 x 6 =       ",	L"6 x 7 =       ",	L"7 x 6 =       ",
+		L"6 x 8 =       ",	L"8 x 6 =       ",	L"6 x 9 =       ",	L"9 x 6 =       ",
+		L"7 x 7 =       ",	L"7 x 8 =       ",	L"8 x 7 =       ",	L"7 x 9 =       ",
+		L"9 x 7 =       ",	L"8 x 8 =       ",	L"8 x 9 =       ",	L"9 x 8 =       ",
+		L"9 x 9 =       "
+	};
+
+	for (int i = 0; i < cols * rows; i++)
+	{
+		formulas.Add(formula_single_plus_single[rand() % 81]);
+	}
+
+	return true;
+}
+
+static bool getMultiPlusSingle(CStringArray& formulas)
+{
+	for (int i = 0; i < cols * rows; i++)
+	{
+		CString formula;
+		formula.Format(L"%d x %d =      ", (rand() % 90) + 10, rand() % 10);
+		formulas.Add(formula);
+	}
+
+	return true;
+}
+
+BOOL CqcDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	m_formula.RemoveAll();
+
+	FormulaType ft = (FormulaType)((CWinAppEx*)AfxGetApp())->GetInt(L"Type");
+	switch (ft)
+	{
+	case eSinglePlusSingle:
+		getSinglePlusSingle(m_formula);
+		break;
+	case eMultiPlusSingle:
+		getMultiPlusSingle(m_formula);
+		break;
+	case eSinglePlusMulti:
+		break;
+	case eMultiDivideSingle:
+		break;
+	default:
+		break;
+	}
+
+	return TRUE;
+}
+
+
+
+
+// CqcDoc 序列化
+
+void CqcDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 缩略图的支持
+void CqcDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 修改此代码以绘制文档数据
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 搜索处理程序的支持
+void CqcDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 从文档数据设置搜索内容。
+	// 内容部分应由“;”分隔
+
+	// 例如:     strSearchContent = _T("point;rectangle;circle;ole object;")；
+	SetSearchContent(strSearchContent);
+}
+
+void CqcDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CqcDoc 诊断
+
+#ifdef _DEBUG
+void CqcDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CqcDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CqcDoc 命令
---
+++ qcView.cpp
@@ -0,0 +1,184 @@
+﻿
+// qcView.cpp: CqcView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "qc.h"
+#endif
+
+#include "qcDoc.h"
+#include "qcView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CqcView
+
+IMPLEMENT_DYNCREATE(CqcView, CView)
+
+BEGIN_MESSAGE_MAP(CqcView, CView)
+	ON_WM_STYLECHANGED()
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CqcView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CqcView 构造/析构
+
+CqcView::CqcView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CqcView::~CqcView()
+{
+}
+
+BOOL CqcView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+void CqcView::OnDraw(CDC* pDC)
+{
+	CqcDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+
+	if (pDoc->m_formula.IsEmpty())
+	{
+		return;
+	}
+
+	LOGFONT lf = { 0 };
+	lf.lfHeight = -MulDiv(18, -pDC->GetDeviceCaps(LOGPIXELSY), 72);
+	lf.lfCharSet = GB2312_CHARSET;
+	wcscpy_s(lf.lfFaceName, LF_FACESIZE, L"微软雅黑");
+	CFont font;
+	font.CreateFontIndirect(&lf);
+	CFont* pOldFont = (CFont*)pDC->SelectObject(&font);
+	pDC->SetBkMode(TRANSPARENT);
+
+	CRect rc(0, 0, 35565, 35565);
+	pDC->DrawText(pDoc->m_formula.GetAt(0), &rc, DT_SINGLELINE | DT_CALCRECT);
+	int w = (int)(rc.Width() * 1.1f);
+	int h = (int)(rc.Height() * 1.3f);
+
+	CSize sz;
+	if (pDC->IsPrinting())
+	{
+		sz.cx = pDC->GetDeviceCaps(HORZRES);
+		sz.cy = pDC->GetDeviceCaps(VERTRES);
+	}
+	else
+	{
+		CRect rect;
+		GetClientRect(&rect);
+		sz.cx = rect.Width();
+		sz.cy = rect.Height();
+	}
+
+	int left = (sz.cx - w * cols) / 2;
+	int top = (sz.cy - h * rows) / 2;
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			pDC->TextOut(left + j * w, top, pDoc->m_formula.GetAt(i * cols + j));
+		}
+		top += h;
+	}
+
+	pDC->SelectObject(pOldFont);
+}
+
+
+void CqcView::OnInitialUpdate()
+{
+	CView::OnInitialUpdate();
+
+
+	// TODO: 调用 GetListCtrl() 直接访问 ListView 的列表控件，
+	//  从而可以用项填充 ListView。
+}
+
+
+// CqcView 打印
+
+
+void CqcView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CqcView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CqcView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CqcView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CqcView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CqcView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CqcView 诊断
+
+#ifdef _DEBUG
+void CqcView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CqcView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CqcDoc* CqcView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CqcDoc)));
+	return (CqcDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CqcView 消息处理程序
+void CqcView::OnStyleChanged(int nStyleType, LPSTYLESTRUCT lpStyleStruct)
+{
+	//TODO: 添加代码以响应用户对窗口视图样式的更改
+	CView::OnStyleChanged(nStyleType,lpStyleStruct);
+}
